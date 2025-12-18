dcc8e637ac02b1c2085e4c436b981cd5cc5d854b
usha-2006 <ushapasapala2006@gmail.com>
2025-12-18 20:29:29 +0530

    Create TrianglePattern.java

---
+++ TrianglePattern.java
@@ -0,0 +1,15 @@
+public class TrianglePattern{
+    public static void main(String args[]){
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=4-1;j++){
+                for(int k=1;k<=2*i-1;k++){
+                    System.out.print("*");
+
+                }
+                System.out.println("");
+            }
+        }
+    }
+
+
+}
