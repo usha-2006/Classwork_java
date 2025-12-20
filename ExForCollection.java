3a8f88bf2e5d96506a7079531c7edf8ece82abe2
usha-2006 <ushapasapala2006@gmail.com>
2025-12-20 14:19:30 +0530

    Create  ExForCollection.java

---
+++ ExForCollection.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+
+public class ExForCollection {
+    public static void main(String[] args) {
+        List<Integer> l =new ArrayList<Integer>();
+        /*List l1 =new Vector();
+        List l2 =new Stack();
+        List l3 =new LinkedList();*/
+        l.add(10 );
+        l.add(20);
+        l.add(30);
+        l.add(40 );
+        System.out.println("List :  " +l);
+        System.out.println(".............................");
+        ArrayList<Integer> al=new ArrayList<Integer>();
+        al.add(10 );
+        al.add(20);
+        al.add(30);
+        al.add(40 );
+        System.out.println("ArrayList" + al);
+        
+    }
+    
+}
