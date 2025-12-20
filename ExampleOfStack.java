69291f5b03847dac761073b2cb736ad137028646
usha-2006 <ushapasapala2006@gmail.com>
2025-12-20 14:16:46 +0530

    Create  ExampleOfStack.java

---
+++ ExampleOfStack.java
@@ -0,0 +1,29 @@
+package day6_crt;
+
+import java.util.*;
+
+
+
+
+public class ExampleOfStack {
+
+	public static void main(String[] args) {
+		
+		Stack<Integer> s= new Stack<Integer>();
+		s.push(10);
+		s.push(30);
+		s.push(70);
+		s.push(24);
+		
+		System.out.println(s);
+		
+		System.out.println(s.pop());
+		
+		System.out.println(s);
+		
+		
+		
+
+	}
+
+}
